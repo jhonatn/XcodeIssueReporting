939059519ddd3833036e9565e3c86418dfc3924c
Jhonatan A <1548170+baguio@users.noreply.github.com>
2022-07-18 21:06:30 -0500

    Use unsigned numbers for locations

--- Sources/XcodeIssueReporting/IssueLocation.swift
+++ Sources/XcodeIssueReporting/IssueLocation.swift
@@ -11,9 +11,9 @@ public struct XcodeIssueLocation {
     /// The file path on disk for this location.
     public let file: String
     /// The line offset in the file for this location. 1-indexed.
-    public let line: Int?
+    public let line: UInt?
     /// The character offset in the file for this location. 1-indexed.
-    public let column: Int?
+    public let column: UInt?
 }
 
 extension XcodeIssueLocation {
@@ -21,11 +21,11 @@ extension XcodeIssueLocation {
         self.init(file: absoluteFilePath, line: nil, column: nil)
     }
     
-    public static func sourceCodeFile(_ absoluteFilePath: String, line: Int) -> Self {
+    public static func sourceCodeFile(_ absoluteFilePath: String, line: UInt) -> Self {
         self.init(file: absoluteFilePath, line: line, column: nil)
     }
     
-    public static func sourceCodeFile(_ absoluteFilePath: String, line: Int, column: Int) -> Self {
+    public static func sourceCodeFile(_ absoluteFilePath: String, line: UInt, column: UInt) -> Self {
         self.init(file: absoluteFilePath, line: line, column: column)
     }
 }
